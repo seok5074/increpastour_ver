01659861203156ebee0265ba8398714f5eb1dd63
AAAAAAAAAAhn <bojjiha@gmail.com>
2019-10-31 14:16:09 +0900

    src main

--- increpastourjang/src/main/webapp/resources/board/textbox.js
+++ increpastourjang/src/main/webapp/resources/board/textbox.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   var placeholderTarget = $('.textbox input[type="text"], .textbox input[type="password"]');
   
+  // placeholderTarget가 빈 값이 아니면 뒤에 글이 가리기 때문에 숨김 처리
+  if(placeholderTarget.val() != null){
+	  $(placeholderTarget).siblings('label').fadeOut('fast');
+  }
+  
   //포커스시
   placeholderTarget.on('focus', function(){
     $(this).siblings('label').fadeOut('fast');
@@ -12,4 +17,6 @@ $(document).ready(function() {
       $(this).siblings('label').fadeIn('fast');
     }
   });
+  
+  
 });
